60fff2cb879357bb754f0b73b34e5ecb401d9b19
sk-etcher <sophiakee7@gmail.com>
2024-02-26 15:13:20 +1100

    interactive index.html

---
+++ scripts/landing-animation.js
@@ -0,0 +1,36 @@
+//this interactive landing page is built upon a project I did in the Digital Craft course.
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    frameRate(10);
+      textAlign(CENTER);
+  }
+  
+  let r;
+  let g;
+  let b;
+  //referred to https://p5js.org/reference/#/p5/pmouseX
+  
+  function draw() {
+    
+    fill(0);
+  noStroke();
+    textFont("Courier New");
+    textSize(36);
+    text("Click Anywhere to Enter", width/2, height/2);
+    
+    r = random(0,255);
+    g = random(0,255);
+    b = random(0,255);
+    
+    stroke(r,g,b);
+    strokeWeight(5);
+    
+  line(mouseX, mouseY, pmouseX, pmouseY);
+    print(pmouseX + ' -> ' + mouseX);
+    
+  }
+  //open in the same window
+    function mouseClicked(){
+      window.open("https://skfolio.glitch.me/homepage.html","_self");
+    }
--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,13 @@ $(document).ready(function() {
     $("#bottomBar").load("/portfolio/headers.html #footbar");
     $("#darkModeTopBar").load("/portfolio/headers.html #darkmodeheadbar");
 });
+
+var labeltext = document.getElementsByClassName('projectThumb').innerText;
+
+labeltext.style.display = "none";
+
+function showlabels(){
+  if (labeltext.style.display === "none")
+  {labeltext.style.display = "block";}
+  else {labeltext.style.display = "none"}
+}
